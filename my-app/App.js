d01a8897f5d60687b2774ee1d6f62c393d3293ab
Eric Ziouane <ericziouane@gmail.com>
2023-09-03 23:23:54 +0200

    added pages to navigator

--- my-app/App.js
+++ my-app/App.js
@@ -3,6 +3,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import DuelScreen from './src/screens/DuelScreen';
+import DashboardScreen from './src/screens/DashboardScreen';
+
 
 const Stack = createStackNavigator(); // This was missing
 
@@ -17,10 +22,10 @@ function HomeScreen() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        {/* You can add more screens here as needed */}
-      </Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Duel" component={DuelScreen} />
+      <Stack.Screen name="Dashboard" component={DashboardScreen} />
     </NavigationContainer>
   );
 }
